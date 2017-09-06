f1d37e68a8861dc6c089d5a94ed5ef61e1e2cc5b
Tsai-Ting Yang <u9934007@gmail.com>
2017-09-06 13:56:32 +0800

    remove storyboard

--- VideoPlayer/VideoPlayer/ViewController.swift
+++ VideoPlayer/VideoPlayer/ViewController.swift
@@ -12,14 +12,8 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = UIColor.red
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
