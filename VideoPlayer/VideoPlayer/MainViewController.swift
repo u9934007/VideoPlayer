58fdc1f8ec20ec686f78e239a0180b6aeb0a8c45
Tsai-Ting Yang <u9934007@gmail.com>
2017-09-06 14:11:14 +0800

    add app icon

--- VideoPlayer/VideoPlayer/MainViewController.swift
+++ VideoPlayer/VideoPlayer/MainViewController.swift
@@ -7,16 +7,26 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class MainViewController: UIViewController {
     
     var playerLayer: AVPlayerLayer?
+    let aVPlayer = AVPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = UIColor.red
 
+    }
+    
+    override func viewWillLayoutSubviews() {
+        
+        super.viewWillLayoutSubviews()
+        playerLayer?.frame = self.view.bounds
+    
     }
 
 }
